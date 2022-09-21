068be62ddac56e65be8a7785cb08a5691234788f
Nutthawut Kongsopa <nutthawutkongsopa@gmail.com>
2022-09-21 17:48:18 +0700

    storybook

---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+import mytheme from "./theme"
+
+addons.setConfig({
+    theme: mytheme
+});
---
+++ .storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'NS React Block UI',
+    //brandImage: 'https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png',
+});
