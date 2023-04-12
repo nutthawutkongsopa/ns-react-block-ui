a5c389ba919683fa0825068180a4638a5335de1c
Nutthawut Kongsopa <nutthawutkongsopa@gmail.com>
2023-04-12 11:03:13 +0700

    setBlockUIDefaultProps

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 
-export { BlockUI } from "./BlockUI"
+export { BlockUI, setBlockUIDefaultProps } from "./BlockUI"
 export { BlockUIContext, BlockUIProvider, useBlockUIContext } from "./BlockUIContext"
